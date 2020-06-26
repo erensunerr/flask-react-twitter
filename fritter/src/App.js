bbe8cf680adc2f5fb2280be6e77364cc7a4939fa
Eren Suner <gerensuner@gmail.com>
2020-06-25 21:15:36 -0400

    removed unnecessary

--- fritter/src/App.js
+++ fritter/src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-    var a;
-   fetch('/time').then(res => res.json()).then(a)
-   console.log(a)
-
+  fetch('/time')
+  .then(response => response.json())
+  .then(commits => );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {a.time}.</p>
-      </header>
-    </div>
-  );
+    <h1>
+      HELLO, time = {x}
+    </h1>
+  )
+
 }
 
 export default App;
